2b3d198e69b0f5a9afb26404487ee9a175270893
dhruvguptaTTN <dhruv.gupta@tothenew.com>
2025-05-21 20:26:48 +0530

    Update _outputs.tf

--- _outputs.tf
+++ _outputs.tf
@@ -1,14 +1,19 @@
 output "zone_ids" {
-  description = "Map of Route53 zone names to their IDs"
-  value       = module.zone.zone_ids
+  description = "Map of created Route53 zone IDs"
+  value       = module.zone.route53_zone_zone_id
 }
 
 output "zone_names" {
-  description = "Map of Route53 zone names to their names (fully qualified)"
-  value       = module.zone.zone_names
+  description = "Map of created Route53 zone names"
+  value       = module.zone.route53_zone_name
+}
+
+output "record_names" {
+  description = "Map of created Route53 record names"
+  value       = module.records.route53_record_name
 }
 
 output "record_fqdns" {
-  description = "Map of record keys to the FQDNs of the created records"
-  value       = module.records.record_fqdns
+  description = "Map of created Route53 record FQDNs"
+  value       = module.records.route53_record_fqdn
 }
