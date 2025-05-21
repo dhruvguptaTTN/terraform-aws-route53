c82c5b150d32b8d7520d12c528c351ce4a6cc3dd
dhruvguptaTTN <dhruv.gupta@tothenew.com>
2025-05-21 20:13:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-module "zones" {
-  source = "./zone"
+module "zone" {
+  source = "./modules/zone"
 
-  create = var.create
+  create = var.zone_create
   zones  = var.zones
-  tags   = var.tags
+  tags   = var.zone_tags
 }
 
 module "records" {
-  source = "./records"
+  source = "./modules/records"
 
-  create               = var.create
-  zone_id              = var.zone_id
-  zone_name            = var.zone_name
-  private_zone         = var.private_zone
-  records              = var.records
-  records_jsonencoded  = var.records_jsonencoded
+  create              = var.record_create
+  zone_id             = var.zone_id != null ? var.zone_id : (length(module.zone.route53_zone_zone_id) > 0 ? values(module.zone.route53_zone_zone_id)[0] : null)
+  zone_name           = var.zone_name != null ? var.zone_name : (length(module.zone.route53_zone_name) > 0 ? values(module.zone.route53_zone_name)[0] : null)
+  private_zone        = var.private_zone
+  records             = var.records
+  records_jsonencoded = var.records_jsonencoded
 }
