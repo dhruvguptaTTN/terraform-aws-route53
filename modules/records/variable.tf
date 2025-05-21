4f0243e27a816d5dec06e4d92e96d621ed15ca98
dhruvguptaTTN <dhruv.gupta@tothenew.com>
2025-05-21 16:37:35 +0530

    Update variable.tf

--- modules/records/variable.tf
+++ modules/records/variable.tf
@@ -1,8 +1,35 @@
+variable "create" {
+  description = "Whether to create DNS records"
+  type        = bool
+  default     = true
+}
 
-variable "recordName" {}
+variable "zone_id" {
+  description = "ID of DNS zone"
+  type        = string
+  default     = null
+}
 
-variable "zone_id" {}
+variable "zone_name" {
+  description = "Name of DNS zone"
+  type        = string
+  default     = null
+}
 
-variable "createRecord" {}
+variable "private_zone" {
+  description = "Whether Route53 zone is private or public"
+  type        = bool
+  default     = false
+}
 
-variable "zone_name" {}
+variable "records" {
+  description = "List of objects of DNS records"
+  type        = any
+  default     = []
+}
+
+variable "records_jsonencoded" {
+  description = "List of map of DNS records (stored as jsonencoded string, for terragrunt)"
+  type        = string
+  default     = null
+}
