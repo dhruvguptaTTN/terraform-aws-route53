5938073c5d33ff6e032af320e4abaca1c9f3e94f
dhruvguptaTTN <dhruv.gupta@tothenew.com>
2025-05-21 16:43:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,18 @@
-module "zone" {
-    source        = "./modules/zone"
-    createZone    = true
-    zones = {
-        "private-zone" = {
-            domain_name = "test.internal.com"
-            vpc = [
-                {
-                vpc_id = "vpc-09aceeb53e4bd8994"
-                },
-            ]
-            tags = {
-                Name = "private-zone"
-            }
-        }
-    }
-    tags = {
-        ManagedBy = "terraform"
-    }
+module "zones" {
+  source = "./zone"
+
+  create = var.create
+  zones  = var.zones
+  tags   = var.tags
 }
 
 module "records" {
-    source                  = "./modules/records"
-    createRecord            = false
-    zone_id                 = "module.zone.route53_zone_id.private-zone" # If you already have hosted zone use hosted zone id
-    zone_name               = "module.zone.route53_zone_name.private-zone" # If you already have hosted zone use hosted zone name
-    recordName = [
-        {
-            name = "test"
-            type = "A"
-            ttl  = 3600
-            records = ["10.10.10.10"]
-        },
-        {
-            name = "test2"
-            type = "A"
-            ttl  = 3600
-            records = ["10.10.10.20"]
-        },
+  source = "./records"
 
-    ]
+  create               = var.create
+  zone_id              = var.zone_id
+  zone_name            = var.zone_name
+  private_zone         = var.private_zone
+  records              = var.records
+  records_jsonencoded  = var.records_jsonencoded
 }
